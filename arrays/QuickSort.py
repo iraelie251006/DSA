390165b9bdce7ee85df8f344111c7ce0ae5053c1
Elie <niyubwayoiraelie5777@gmail.com>
2026-01-30 23:50:18 +0200

    Implemented quicksort function

--- arrays/QuickSort.py
+++ arrays/QuickSort.py
@@ -10,3 +10,15 @@ def partition(array, low, high):
     array[i+1], array[high] = array[high], array[i+1]
     return i+1
 
+def quicksort(array, low=0, high=None):
+    if high is None:
+        high = len(array) - 1
+
+    if low < high:
+        pivot_index = partition(array, low, high)
+        quicksort(array, low, pivot_index-1)
+        quicksort(array, pivot_index+1, high)
+
+my_array = [64, 34, 25, 12, 22, 11, 90, 5]
+quicksort(my_array)
+print("Sorted array:", my_array)

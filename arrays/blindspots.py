d91d99c4359f5890d7883433fe852ea69cc6fa0b
Elie <niyubwayoiraelie5777@gmail.com>
2026-02-13 22:52:05 +0200

    Started list of must know

--- arrays/blindspots.py
+++ arrays/blindspots.py
@@ -4,3 +4,6 @@ arr = [2, 1, 4, 3, 6, 5, 7, 9, 8, 10]
 
 # when working with arrays always remember to check if its empty 
 # first for you to handle edge cases like that.
+
+
+# List of urgent DSA i have to know:

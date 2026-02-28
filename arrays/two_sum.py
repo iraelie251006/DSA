2a939c5ecbdb9c4cb0b86362cc5f631ce9cad93d
Elie <niyubwayoiraelie5777@gmail.com>
2026-02-28 23:56:50 +0200

    created a complement

--- arrays/two_sum.py
+++ arrays/two_sum.py
@@ -1,5 +1,7 @@
 arr = [2, 1, 4, 3, 6, 5, 7, 9, 8, 10]
-
+n = len(arr)
 # Going to re implement two sum problem
 def two_sum(arr, target):
     seen = {}
+    for i in range(n):
+        complement = target - arr[i]

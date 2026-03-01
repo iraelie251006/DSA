803b29669e3f8c4a95b6d9984970e1aab004f59f
Elie <niyubwayoiraelie5777@gmail.com>
2026-03-01 23:09:20 +0200

    Added target description to two_sum function

--- arrays/two_sum.py
+++ arrays/two_sum.py
@@ -1,6 +1,7 @@
 arr = [2, 1, 4, 3, 6, 5, 7, 9, 8, 10]
 
 # Going to re implement two sum problem
+# find two indeces that can add up to target
 def two_sum(arr, target):
     seen = {}
     for i, num in enumerate(arr):
